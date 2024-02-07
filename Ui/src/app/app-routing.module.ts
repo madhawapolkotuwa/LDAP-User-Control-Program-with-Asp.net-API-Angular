2258c10f2d23bd0c8937a8f2bcc87862deb6204f
madhawa polkotuwa <32560614+madhawapolkotuwa@users.noreply.github.com>
2024-02-07 09:22:18 +0900

    Update app-routing.module.ts

--- Ui/src/app/app-routing.module.ts
+++ Ui/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, mapToCanActivate } from '@angular/router';
 import { LoginComponent } from './Components/login/login.component';
 import { MainPageComponent } from './Components/main-page/main-page.component';
+import { AuthService } from './Services/auth.service';
 
 const routes: Routes = [
   {path:'',component: LoginComponent},
-  {path:'main',component:MainPageComponent}
+  {path:'main',component:MainPageComponent, canActivate:mapToCanActivate([AuthService])}
 ];
 
 @NgModule({
